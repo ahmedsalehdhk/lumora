d35c754f7aaee554446e4143d70a379751109009
ahmedsalehdhk <ahmedsalehdhk@gmail.com>
2025-05-15 12:17:59 +0600

    Instantiated quest posting form

--- app/(pages)/quests/page.tsx
+++ app/(pages)/quests/page.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Form from 'next/form'
 
 const Quests = () => {
   return (
     <div>
       All quests
+      <Form action="" className='bg-gray-300 w-1/2 p-2'>
+        <input type="text" />
+      </Form>
     </div>
   )
 }
