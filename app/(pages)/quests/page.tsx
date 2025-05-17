b59e2c6fc9aa7f9b6b51ddda3a17689167487024
ahmedsalehdhk <ahmedsalehdhk@gmail.com>
2025-05-17 16:32:34 +0600

    Updated the create quest form

--- app/(pages)/quests/page.tsx
+++ app/(pages)/quests/page.tsx
@@ -1,15 +1,33 @@
-import React from 'react'
-import Form from 'next/form'
+import React from "react";
+import Form from "next/form";
 
 const Quests = () => {
   return (
     <div>
       All quests
-      <Form action="" className='bg-gray-300 w-1/2 p-2'>
-        <input type="text" />
+      <Form action="" className="bg-gray-300 w-1/2 px-24 py-12 flex flex-col gap-5">
+        <input type="text" name="title" placeholder="Title" />
+        <input type="text" name="description" placeholder="Description" />
+        <select name="complexity" id="">
+          <option value="">Complexity</option>
+          <option value="">Easy</option>
+          <option value="">Medium</option>
+          <option value="">Hard</option>
+          <option value="">Insane</option>
+        </select>
+        <select name="repeat" id="">
+          <option value="">None</option>
+          <option value="">Daily</option>
+          <option value="">Weekly</option>
+          <option value="">Monthly</option>
+          <option value="">Yearly</option>
+        </select>
+        <button type="submit" className="bg-white">
+          Activate Quest
+        </button>
       </Form>
     </div>
-  )
-}
+  );
+};
 
-export default Quests
+export default Quests;
